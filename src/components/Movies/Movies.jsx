81840a7c89445ba1b060bd10e81d15910e7da5c7
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2023-10-09 14:50:38 +0300

    Фильмы

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Movies.css';
+import { SearchForm } from '../SearchForm/SearchForm';
+import { Footer } from '../Footer/Footer';
+import { MoviesCardList } from '../MoviesCardList/MoviesCardList';
+
+export const Movies = ({ movies }) => {
+
+	return (
+		<div className="movies">
+			<div className='form-container'>
+				<SearchForm></SearchForm>
+			</div>
+			<MoviesCardList
+				movies={movies}>
+			</MoviesCardList>
+			<Footer></Footer>
+		</div>
+	);
+}
